4aba936ade0e23bf26f091fb0388eedf76d2ff29
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-02 14:25:51 +0200

    postService

---
+++ src/services/postService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+class PostService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}/post`,
+    });
+  }
+
+  getPosts() {
+    return this.api.get('/').then(({ data }) => data).catch(err => console.error(err));
+  }
+
+  getPost(id) {
+    return this.api.get(`/${id}`).then(({ data }) => data).catch(err => console.error(err))
+  }
+
+  deletePost(id) {
+    return this.api.delete(`/${id}`).then(({ data }) => data).catch(err => console.error(err))
+  }
+
+  editPost(id, body) {
+    return this.api.put(`/${id}`, body).then(({ data }) => data).catch(err => console.error(err))
+  }
+
+  createPost(body) {
+    return this.api.post('/', body).then(({ data }) => data).catch(err => console.error(err))
+  }
+
+}
+
+const postService = new PostService ();
+
+export default postService;
