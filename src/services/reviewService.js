b92de21dcab6b080da24d703e204812f7adeb10e
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-02 14:21:15 +0200

    reviewService

---
+++ src/services/reviewService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+class ReviewService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}/review`,
+    });
+  }
+
+  getReviews() {
+    return this.api.get('/').then(({ data }) => data).catch(err => console.error(err));
+  }
+
+  getReview(id) {
+    return this.api.get(`/${id}`).then(({ data }) => data).catch(err => console.error(err))
+  }
+
+  deleteReview(id) {
+    return this.api.delete(`/${id}`).then(({ data }) => data).catch(err => console.error(err))
+  }
+
+  editReview(id, body) {
+    return this.api.put(`/${id}`, body).then(({ data }) => data).catch(err => console.error(err))
+  }
+
+  createReview(body) {
+    return this.api.post('/', body).then(({ data }) => data).catch(err => console.error(err))
+  }
+
+}
+
+const reviewService = new ReviewService ();
+
+export default reviewService;
