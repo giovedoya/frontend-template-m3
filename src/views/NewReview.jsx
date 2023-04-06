9df1a2ab94a1da7fef166f3a88237dab02f84822
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-06 08:15:05 +0200

    reviews standby

--- src/views/NewReview.jsx
+++ src/views/NewReview.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import reviewService from '../services/reviewService';
 
 function Star({ selected, onClick }) {
@@ -11,6 +11,7 @@ export default function NewReview() {
     rating: '',
     comment: '',
   };
+  const { dressId } = useParams();
   const [newReview, setNewReview] = useState(initialState);
   const [error, setError] = useState('');
   const [selectedStars, setSelectedStars] = useState(0);
@@ -38,13 +39,14 @@ export default function NewReview() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const reviewNew = await reviewService.createReview(newReview);
-      console.log('datos encontrados', reviewNew);
+      const reviewNew = await reviewService.createReview({...newReview, dressId: dressId});
+
       if (reviewNew && reviewNew._id) {
         setError('');
         navigate(`/review/${reviewNew._id}`);
         setNewReview(initialState);
       } else {
+
       }
     } catch (err) {
       console.error(err);
@@ -78,90 +80,3 @@ export default function NewReview() {
     </div>
   );
 }
-
-
-
-
-
-// import React, { useState } from 'react'
-// import { useNavigate } from "react-router-dom";
-// import reviewService from "../services/reviewService";
-// import { FaStar, FaRegStar } from 'react-icons/fa';
-
-// export default function NewReview() {
-//     const initialState = {
-//         rating: "",
-//         comment: "",
-//     };
-   
-//     const [ newReview, setNewReview] = useState(initialState);
-//     const [ error, setError] = useState("");
-//     const navigate = useNavigate();
-
-//     const handleChange = (e) => {
-//         setNewReview((prev) => {
-//           return {
-//             ...prev,
-//             [e.target.name]: e.target.value,
-//           };
-//         });
-//       };
-
-//       const ratingStars = (rating) => {
-//         const stars = [];
-//         for (let i = 0; i < 5; i++) {
-//           if (i < rating) {
-//             stars.push(<FaStar key={i} />);
-//           } else {
-//             stars.push(<FaRegStar key={i} />);
-//           }
-//         }
-//         return stars;
-//       };
-
-//       const handleSubmit = async (e) => {    
-//         e.preventDefault();
-//         try {
-            
-//           const reviewNew = await reviewService.createReview(newReview);     
-//             console.log('datos encontrados', reviewNew)
-//           if (reviewNew && reviewNew._id) {
-//             setError("");
-//             navigate(`/review/${reviewNew._id}`);
-//             setNewReview(initialState);
-//           } else {
-//           }
-//         } catch (err) {
-//           console.error(err);
-//           setError(err);
-//         }
-//       };      
-
-//   return (
-//     <div>
-//       <h2>Post new</h2>
-//       <form onSubmit={handleSubmit}>
-//         {error && <p>Something went wrong. Couldn't find your review</p>}
-//         <div>
-//           <label>Rating</label>
-//           <div>
-//             {ratingStars(newReview.rating)}
-//           </div>
-//         </div>
-//         <div>
-//           <label>Comment</label>
-//           <textarea
-//             type="text"
-//             name="comment"
-//             value={newReview.comment}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-//         <button type="submit">
-//           Save changes
-//         </button>
-//       </form>
-//     </div>
-//   )
-// }
