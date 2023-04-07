5af15dcd5e0001fa9297c51ff147defc3315d03e
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-07 17:24:13 +0200

    ProfileView create

---
+++ src/views/ProfileViews.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import profileService from '../services/profileService';
+
+export default function ProfileViews() {
+    const { user } = useContext(AuthContext);
+    const [profile, setProfile] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+
+ const getProfile = async () => {
+    try {
+      const response = await profileService.getProfile();
+      setProfile(response);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getProfile()
+  }, []) 
+
+  return (
+    <div>
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && (
+        <div>
+          <h3>{profile.username}</h3>
+          <p>Email: {user.email}</p>
+        </div>
+      )}
+      <Link to="/profile/edit">Edit profile</Link>
+    </div>
+  );
+}
