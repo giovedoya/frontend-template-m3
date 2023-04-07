fa6b36764fede1d35d0282fa9fca657baca38038
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-07 09:29:12 +0200

    original version home and searchDress

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import dressService from "../services/dressService";
 // import { Link } from 'react-router-dom'
 import Card from "../components/Card";
-import { useAuth } from "../hooks/useAuth";
 import SearchDress from "../components/SearchDress";
 
 
 export default function Home() {
   const [dresses, setDress] = useState([]);
-  const {  user } = useAuth();
+
+
+  // all dresses 
   const getDresses = async () => {
     try {
       const response = await dressService.getDresses();
@@ -21,9 +22,7 @@ export default function Home() {
   useEffect(() => {
     getDresses();
   }, []);
-if (user) {
 
-}
   return (
 <div className="flex gap-4">
     <SearchDress />
