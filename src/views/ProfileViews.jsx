f7576fc25cc7c880d9d69c8f3bdefd2d87143bf3
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-07 20:41:31 +0200

    profileView

--- src/views/ProfileViews.jsx
+++ src/views/ProfileViews.jsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react';
-// import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import profileService from '../services/profileService';
 
 export default function ProfileViews() {
-    const { user } = useContext(AuthContext);
-    const [profile, setProfile] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
+  const { user } = useContext(AuthContext);
+  const [profile, setProfile] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-
- const getProfile = async () => {
+  const getProfile = async () => {
     try {
       const response = await profileService.getProfile();
       setProfile(response);
@@ -20,8 +18,8 @@ export default function ProfileViews() {
   };
 
   useEffect(() => {
-    getProfile()
-  }, [profile]) 
+    getProfile();
+  }, []);
 
   return (
     <div>
