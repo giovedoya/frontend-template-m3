4cf45b4fd898dbcb57de574094e9638178f7fd37
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-09 16:54:49 +0200

    basic post component styles

--- src/components/PostCards.jsx
+++ src/components/PostCards.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export default function PostCard(props) {
   const { posts } = props;
     return (
-<div>
- <h2 >
-    <Link to={`/post/${posts._id}`} >{posts.title}</Link>
+<div className="bg-white p-4 rounded-lg shadow-lg">
+  <img className="w-full h-auto mb-4" src={posts.image} alt={posts.title} />
+  <h2 className="text-lg font-bold mb-2">
+    <Link to={`/post/${posts._id}`} className="text-blue-500 hover:text-blue-700">{posts.title}</Link>
   </h2>
- <p>{posts.content}</p>
- </div>
-  );
-}
+  <p className="text-gray-700">{posts.content.substring(0, 100)}...</p>
+</div>
+    )}
