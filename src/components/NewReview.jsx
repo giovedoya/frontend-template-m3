ac3903e97bab5e5b4af902847347ed0cc8df8155
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-09 16:20:54 +0200

    basic styles of review component

--- src/components/NewReview.jsx
+++ src/components/NewReview.jsx
@@ -41,11 +41,15 @@ export default function NewReview() {
   
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        {error && <p>Something went wrong. Couldn't find your review</p>}
-        <label>Rating</label>      
+    <div className="flex flex-col justify-center items-center">
+    <form className="w-full max-w-md" onSubmit={handleSubmit}>
+      {error && <p className="text-red-500 mb-4">Something went wrong. Couldn't find your review</p>}
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="rating">
+          Rating
+        </label>      
         <input 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="number"
           name="rating" 
           min="1"
@@ -53,17 +57,26 @@ export default function NewReview() {
           value={newReview.rating} 
           onChange={handleChange}
           required
-        />        
-        <label>Comment</label>
+        /> 
+      </div>     
+      <div className="mb-6">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="comment">
+          Comment
+        </label>
         <textarea
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="comment"
           value={newReview.comment}
           onChange={handleChange}
           required
         />
-        <button type="submit">Save changes</button>
-      </form>
-    </div>
-  );
-}
+      </div>
+      <div className="flex items-center justify-between">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          Save changes
+        </button>
+      </div>
+    </form>
+  </div>
+  )}  
