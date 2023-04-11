ed8210e97a6ff69b824aa30d89d9acc888516aa7
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-11 06:07:25 +0200

    update new post

--- src/views/NewPost.jsx
+++ src/views/NewPost.jsx
@@ -108,6 +108,13 @@ export default function NewPost() {
               onChange={(e) => handleFileUpload(e)}
               className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
             />
+            <input
+              type="file"
+              name="image"
+              id="image"
+              onChange={(e) => handleFileUpload(e)}
+              className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+            />
           </div>
           <div className="flex flex-col space-y-2">
             <label htmlFor="content" className="font-bold text-gray-700">
