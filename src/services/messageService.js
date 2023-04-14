642410cc9d564c2f799b0dd591a002d8e25c56d7
Giovanni Bedoya <giovedoya@gmail.com>
2023-04-14 15:24:11 +0200

    creation of the message service

---
+++ src/services/messageService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+class DressService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}/message`,
+    });
+
+    this.api.interceptors.request.use(config => {
+      const storedToken = localStorage.getItem('authToken');
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+      return config;
+    });
+  }
+
+  getMessages() {
+    return this.api.get('/').then(({ data }) => data).catch(err => console.error(err));
+  }
+
+
+  createMessage(body) {
+    return this.api.post('/', body).then(({ data }) => data).catch(err => console.error(err))
+  }
+
+}
+
+const dressService = new DressService();
+
+export default dressService;
